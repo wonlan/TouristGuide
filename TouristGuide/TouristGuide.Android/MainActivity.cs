8943b096c3a10155ad9c25a1cc2b5390a222288c
tomekmaleta.tm@gmail.com <tomekmaleta.tm@gmail.com>
2022-01-09 04:03:35 +0100

    Added Splash Screen

--- TouristGuide/TouristGuide.Android/MainActivity.cs
+++ TouristGuide/TouristGuide.Android/MainActivity.cs
@@ -7,7 +7,11 @@ using Android.OS;
 
 namespace TouristGuide.Droid
 {
-    [Activity(Label = "TouristGuide", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity
+        (Label = "TouristGuide", 
+        Icon = "@mipmap/icon", 
+        Theme = "@style/MainTheme.Launcher",
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ TouristGuide/TouristGuide.Android/SplashActivity1.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouristGuide.Droid
+{
+    [Activity
+        (Label = "SplashActivity1",
+        MainLauncher =true,
+        Theme ="@style/MainTheme.Splash",
+        NoHistory =true,
+        Icon ="@mipmap/icon")]
+    public class SplashActivity1 : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.splash);
+            Task startupWork = new Task(() => { SimulateStartupAsync(); });
+            startupWork.Start();
+            // Create your application here
+        }
+        private async Task SimulateStartupAsync()
+        {
+            await Task.Delay(1800);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+
+        }
+    }
+}
